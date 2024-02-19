bbd12f49da598068ffbd0d5edcc191e4d87d1c64
Maximiliano Romero <maximilianoromerovigo@gmail.com>
2024-02-19 12:55:35 -0300

    first commit

---
+++ Decisiones/Decisiones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo de dato booleano
+            bool decision;
+            decision = true;
+
+            //Operadores de igualdad
+            Console.WriteLine(6 == 6);
+            Console.WriteLine(6 != 6);
+
+            //Operadores de comparacion
+            Console.WriteLine(7 > 6);
+            Console.WriteLine(6 < 7);
+        }
+    }
+}
---
+++ TAREA2/TAREA2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer programa que calcule el perimetro de cualquier poligono regular
+
+            int cantidadLados;
+            double tamanoLado, perimetro;
+
+            Console.WriteLine("Ingrese cantidad de lados del poligono");
+            cantidadLados = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tamaño de uno de los lados");
+            tamanoLado = Convert.ToDouble(Console.ReadLine());
+
+            perimetro = tamanoLado * cantidadLados;
+
+            Console.WriteLine("El perimetro del poligono es de: {0}", perimetro);
+        }
+    }
+}
